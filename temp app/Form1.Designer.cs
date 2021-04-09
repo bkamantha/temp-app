5a332447ddfe9217bbeb1ba623f4af60ab9cdea7
bhanuka kamantha <you@example.com>
2021-04-09 14:29:25 +0530

    Add project files.

---
+++ temp app/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace temp_app
+{
+    partial class TempDisplay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.data_tb = new System.Windows.Forms.TextBox();
+            this.start_button = new System.Windows.Forms.Button();
+            this.stop_button = new System.Windows.Forms.Button();
+            this.save_button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.portname_cb = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // data_tb
+            // 
+            this.data_tb.Location = new System.Drawing.Point(32, 49);
+            this.data_tb.Multiline = true;
+            this.data_tb.Name = "data_tb";
+            this.data_tb.Size = new System.Drawing.Size(310, 338);
+            this.data_tb.TabIndex = 0;
+            // 
+            // start_button
+            // 
+            this.start_button.Location = new System.Drawing.Point(427, 78);
+            this.start_button.Name = "start_button";
+            this.start_button.Size = new System.Drawing.Size(209, 59);
+            this.start_button.TabIndex = 1;
+            this.start_button.Text = "Start";
+            this.start_button.UseVisualStyleBackColor = true;
+            this.start_button.Click += new System.EventHandler(this.start_button_Click);
+            // 
+            // stop_button
+            // 
+            this.stop_button.Location = new System.Drawing.Point(427, 143);
+            this.stop_button.Name = "stop_button";
+            this.stop_button.Size = new System.Drawing.Size(209, 59);
+            this.stop_button.TabIndex = 1;
+            this.stop_button.Text = "Stop";
+            this.stop_button.UseVisualStyleBackColor = true;
+            this.stop_button.Click += new System.EventHandler(this.stop_button_Click);
+            // 
+            // save_button
+            // 
+            this.save_button.Location = new System.Drawing.Point(427, 319);
+            this.save_button.Name = "save_button";
+            this.save_button.Size = new System.Drawing.Size(209, 59);
+            this.save_button.TabIndex = 1;
+            this.save_button.Text = "Save Data";
+            this.save_button.UseVisualStyleBackColor = true;
+            this.save_button.Click += new System.EventHandler(this.save_button_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(364, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Port Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Value";
+            // 
+            // portname_cb
+            // 
+            this.portname_cb.FormattingEnabled = true;
+            this.portname_cb.Items.AddRange(new object[] {
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4"});
+            this.portname_cb.Location = new System.Drawing.Point(427, 33);
+            this.portname_cb.Name = "portname_cb";
+            this.portname_cb.Size = new System.Drawing.Size(209, 21);
+            this.portname_cb.TabIndex = 5;
+            // 
+            // TempDisplay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 419);
+            this.Controls.Add(this.portname_cb);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.save_button);
+            this.Controls.Add(this.stop_button);
+            this.Controls.Add(this.start_button);
+            this.Controls.Add(this.data_tb);
+            this.Name = "TempDisplay";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox data_tb;
+        private System.Windows.Forms.Button start_button;
+        private System.Windows.Forms.Button stop_button;
+        private System.Windows.Forms.Button save_button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox portname_cb;
+    }
+}
+
---
+++ temp app/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports; // IO Port to communicate with Arduino
+
+namespace temp_app
+{
+    public partial class TempDisplay : Form
+    {
+        // define datatypes and  variables
+        private SerialPort Comport;
+
+        private DateTime datetime;
+        private String in_data;
+
+        public TempDisplay()
+        {
+            InitializeComponent();
+        }
+
+        private void start_button_Click(object sender, EventArgs e)
+        {
+            // Start connection with Serial port
+
+            Comport = new SerialPort();
+            Comport.BaudRate = 9600;
+            Comport.PortName = portname_cb.Text;
+            Comport.Parity = Parity.None;
+            Comport.DataBits = 8;
+            Comport.StopBits = StopBits.One;
+            Comport.DataReceived += Comport_DataReceived;
+
+            // open COM Port and start empty text box to disaplay
+
+            try
+            {
+                Comport.Open();
+                data_tb.Text = "";
+            }
+            catch (Exception ex1)
+            {
+                MessageBox.Show(ex1.Message, "COM PORT Error");
+            }
+        }
+
+        private void Comport_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            // Read incoming serial data and read line by line
+
+            in_data = Comport.ReadLine();
+            this.Invoke(new EventHandler(displaydata_event)); // pass that data to displaydata_event
+        }
+
+        private void displaydata_event(object sender, EventArgs e)
+        {
+            // Display Data in textbox
+
+            datetime = DateTime.Now;
+            string time = datetime.Hour + "HH " + ":" + datetime.Minute + "MM " + ":" + datetime.Second + "SS ";
+            data_tb.AppendText(time + "\t \t" + in_data + "\n");
+        }
+
+        private void stop_button_Click(object sender, EventArgs e)
+        {
+            // Stop button event
+
+            try
+            {
+                Comport.Close();
+            }
+            catch (Exception ex2)
+            {
+                MessageBox.Show(ex2.Message, "Error");
+            }
+        }
+
+        private void save_button_Click(object sender, EventArgs e)
+        {
+            // Save all Data event
+
+            try
+            {
+                string pathfile = @"C:\Users\bhanuka kamantha\Desktop\DATA";
+                string filename = "light_data.txt";
+                System.IO.File.WriteAllText(pathfile + filename, data_tb.Text);
+                MessageBox.Show("Data has benn saved" + pathfile, "save File");
+            }
+            catch (Exception ex3)
+            {
+                MessageBox.Show(ex3.Message, "Error Saving DATA");
+            }
+        }
+    }
+}
